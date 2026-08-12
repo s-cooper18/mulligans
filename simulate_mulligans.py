151b6608b08508b8e38e32d7bcc29ed6d05c9d54
Suzannah Cooper <suzannah.cooper@outlook.com>
2026-08-13 08:39:57 +1000

    Fix script functionality

--- simulate_mulligans.py
+++ simulate_mulligans.py
@@ -5,20 +5,24 @@ from src import dataclasses, deck_loader
 HAND_SIZE = 7
 
 
-def maybe_return_cards(
+def choose_cards_to_return(
     hand: dataclasses.Hand, mulligans: int, print_func: Callable
-) -> None:
-    num_cards_can_keep = HAND_SIZE - mulligans
-    while len(hand) > num_cards_can_keep:
-        value = input("Choose card to put back:")
+) -> set[int]:
+    chosen = set()
+    if mulligans == 0:
+        return chosen
+
+    num_cards_can_keep = len(hand) - mulligans
+    value = input(f"Choose {mulligans} card(s) to put back:")
+
+    while len(chosen) > num_cards_can_keep:
         try:
-            option = int(value)
-            card_to_return = hand.return_card(option)
+            this_index = int(value)
+            chosen.add(this_index)
         except ValueError, IndexError:
             print_func("Invalid value\n")
             continue
-
-        print_func(f"Returned {card_to_return}")
+    return chosen
 
 
 def run_mulligans(deck: dataclasses.Deck, print_func: Callable) -> None:
@@ -28,7 +32,7 @@ def run_mulligans(deck: dataclasses.Deck, print_func: Callable) -> None:
     while True:
         print_func(hand.format_choices())
         print_func(
-            f"\nHave taken {mulligans} mulligans(s), {deck.get_num_exiled()} card(s) in exile\n"
+            f"\nHave taken {mulligans} mulligans(s), {deck.get_num_exiled()} card(s) in exile, {len(deck.cards)} in deck\n"
         )
         value = input(
             f"{'Serum (s), ' if hand.has_serum_powder() else ''}Mulligan (m) or Keep (k):"
@@ -40,16 +44,15 @@ def run_mulligans(deck: dataclasses.Deck, print_func: Callable) -> None:
 
         match value:
             case "s":
-                hand = deck.serum_powder(hand._cards)
-                maybe_return_cards(hand, mulligans, print_func)
+                cards_to_return = choose_cards_to_return(hand, mulligans, print)
+                hand = deck.serum_powder(hand, cards_to_return)
                 print_func(f"Exiled {len(hand._cards)} cards\n")
             case "m":
                 hand = deck.mulligan(hand._cards)
                 mulligans += 1
                 print_func("Mulligan\n")
             case "k":
-                maybe_return_cards(hand, mulligans, print_func)
-
+                cards_to_return = choose_cards_to_return(hand, mulligans, print)
                 print_func(
                     f"Kept hand after {mulligans} mulligans with {deck.get_num_exiled()} cards in exile.\n"
                 )
