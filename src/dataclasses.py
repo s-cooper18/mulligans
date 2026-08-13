9855d2f9731dc5e9b93c102295c9be2562bf5946
Suzannah Cooper <suzannah.cooper@outlook.com>
2026-08-13 11:59:17 +1000

    Fix linting

--- src/dataclasses.py
+++ src/dataclasses.py
@@ -1,5 +1,5 @@
-from collections.abc import Collection
 import random
+from collections.abc import Collection
 
 import attrs
 
@@ -22,9 +22,7 @@ class Hand:
         return "\n".join([card.name for card in self._cards])
 
     def format_choices(self) -> str:
-        return "\n".join(
-            [f"({i + 1}) {self._cards[i]}" for i in range(len(self._cards))]
-        )
+        return "\n".join([f"({i + 1}) {self._cards[i]}" for i in range(len(self._cards))])
 
     def has_serum_powder(self) -> bool:
         return any(card.name for card in self._cards if card.name == "Serum Powder")
@@ -34,7 +32,7 @@ class Hand:
 
     def __len__(self) -> int:
         return len(self._cards)
-    
+
     def get_card(self, which: int) -> Card:
         assert which in range(len(self._cards))
         return self._cards[which]
@@ -61,25 +59,27 @@ class Deck:
         self.shuffle()
         return self.draw_cards(STANDARD_HAND_SIZE)
 
-    def serum_powder(
-        self, hand: Hand, items_to_bottom: set[int] | None = None
-    ) -> Hand:
+    def serum_powder(self, hand: Hand, items_to_bottom: set[int] | None = None) -> Hand:
         possible_indices = set(range(len(hand)))
-        
+
         if not items_to_bottom:
             self.exile_cards(hand._cards)
             return self.draw_cards(7)
-            
+
         # are all valid choices
         assert items_to_bottom.intersection(possible_indices) == items_to_bottom
 
-        cards_to_exile = {hand.get_card(i) for i in possible_indices.difference(items_to_bottom)} if items_to_bottom else hand._cards
+        cards_to_exile = (
+            {hand.get_card(i) for i in possible_indices.difference(items_to_bottom)}
+            if items_to_bottom
+            else hand._cards
+        )
 
         cards_to_bottom = {hand.get_card(i) for i in items_to_bottom}
         self.return_cards(cards_to_bottom)
-            
+
         self.exile_cards(cards_to_exile)
-            
+
         return self.draw_cards(len(cards_to_exile))
 
     def __len__(self) -> int:
@@ -87,11 +87,11 @@ class Deck:
 
     def get_num_exiled(self) -> int:
         return len(self.exiled)
-    
+
     def return_cards(self, cards: set[Card]) -> None:
         cards_to_return = list(cards)
         random.shuffle(cards_to_return)
         self.cards.extend(cards_to_return)
-        
+
     def exile_cards(self, cards: Collection[Card]) -> None:
         self.exiled.extend(cards)
